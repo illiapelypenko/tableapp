1f34b90dc02390588b0e1e61d96030caeef02f30
Illia <pelypenkoillya@gmail.com>
2018-10-20 21:39:52 +0300

    before validation

--- src/App.js
+++ src/App.js
@@ -3,50 +3,65 @@ import './App.css';
 import Table from './components/jsComponents/Table.js';
 import Form from './components/jsComponents/Form.js';
 
+// let people =  [
+//   {
+//     id: 0,
+//     firstName: 'Harry',
+//     lastName: 'Potter',
+//     phoneNumber: '0987654321',
+//     age: 11
+//   },
+//   {
+//     id: 1,
+//     firstName: 'Tom',
+//     lastName: 'Riddle',
+//     phoneNumber: '0666993322',
+//     age: 70
+//   },
+//   {
+//     id: 2,
+//     firstName: 'Albus',
+//     lastName: 'Dumbuldor',
+//     phoneNumber: '0336669911',
+//     age: 80
+//   },
+//   {
+//     id: 3,
+//     firstName: 'Sirius',
+//     lastName: 'Black',
+//     phoneNumber: '0123456789',
+//     age: 35
+//   }
+// ];
+//localStorage.setItem('people', JSON.stringify(people));
+
 class App extends Component {
   constructor(){
     super();
     this.state = {
       sortOn: 'default',
-      people: [
-        {
-          id: 0,
-          firstName: 'Harry',
-          lastName: 'Potter',
-          phoneNumber: '0987654321',
-          age: 11
-        },
-        {
-          id: 1,
-          firstName: 'Tom',
-          lastName: 'Riddle',
-          phoneNumber: '0666993322',
-          age: 70
-        },
-        {
-          id: 2,
-          firstName: 'Albus',
-          lastName: 'Dumbuldor',
-          phoneNumber: '0336669911',
-          age: 80
-        },
-        {
-          id: 3,
-          firstName: 'Sirius',
-          lastName: 'Black',
-          phoneNumber: '0123456789',
-          age: 35
-        }
-      ]
+      people: []
     }
     this.handleDeleteRow = this.handleDeleteRow.bind(this);
     this.handleSort = this.handleSort.bind(this);
     this.Sort = this.Sort.bind(this);
+    this.handleAddMan = this.handleAddMan.bind(this);
+  }
+  componentDidMount() {
+    this.updatePeople();
+  }
+  updatePeople(){
+    let people = JSON.parse(localStorage.getItem('people'));
+    for(let man of people){
+      man.id = people.indexOf(man);
+    }
+    this.setState({people});
   }
   handleDeleteRow(man){
     let newPeople = this.state.people.slice();
     newPeople.splice(this.state.people.indexOf(man), 1);
-    this.setState({people: newPeople});
+    localStorage.setItem('people', JSON.stringify(newPeople));
+    this.updatePeople();
   }
   handleSort(prop){
     this.setState({sortOn: prop});
@@ -65,12 +80,22 @@ class App extends Component {
         return a.age - b.age;
     }
   }
+  handleAddMan(man){
+
+    man.id = this.state.people.length+1;
+    let people = this.state.people.slice(0);
+    people.push(man);//проблема тут
+    localStorage.setItem('people', JSON.stringify(people));
+    this.updatePeople();
+    //незапушилось
+  }
   render() {
+    console.log("render");
+    console.log(this.state.people);
     return (
       <div className="App">
-        {console.log(this.state.sortOn)}
         <Table people={this.state.people.sort((a, b)=>{return this.Sort(a, b)})} onDeleteRow={this.handleDeleteRow} onSort={this.handleSort}/>
-        <Form />
+        <Form onAddMan={this.handleAddMan}/>
       </div>
     );
   }
--- src/components/jsComponents/Form.js
+++ src/components/jsComponents/Form.js
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
+import '../cssComponents/Form.css';
 
 class Form extends Component {
+  constructor(){
+    super();
+    this.handleFirstNameInputChange = this.handleFirstNameInputChange.bind(this);
+    this.handleLastNameInputChange = this.handleLastNameInputChange.bind(this);
+    this.handlephoneNumberInputChange = this.handlephoneNumberInputChange.bind(this);
+    this.handleAgeInputChange = this.handleAgeInputChange.bind(this);
+    this.handleAddMan = this.handleAddMan.bind(this);
+    this.state = {
+      id: null,
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      age: ''
+    };
+  }
+  handleFirstNameInputChange(e){
+    this.setState({firstName: e.target.value});
+  }
+  handleLastNameInputChange(e){
+    this.setState({lastName: e.target.value});
+  }
+  handlephoneNumberInputChange(e){
+    this.setState({phoneNumber: e.target.value});
+  }
+  handleAgeInputChange(e){
+    this.setState({age: parseInt(e.target.value)});
+  }
+  handleAddMan(e){
+    this.props.onAddMan(this.state);
+    this.setState({firstName: ''});
+    this.setState({lastName: ''});
+    this.setState({phoneNumber: ''});
+    this.setState({age: ''});
+    e.preventDefault();
+  }
   render() {
     return (
       <div className="Form">
+        <form onSubmit={this.handleAddMan}>
+          <input className='firstNameInput formInput' spellCheck="false" type='text' onChange={this.handleFirstNameInputChange} value={this.state.firstName}></input>
+          <input className='lastNameInput formInput' spellCheck="false" type='text' onChange={this.handleLastNameInputChange} value={this.state.lastName}></input>
+          <input className='phoneNumberInput formInput' spellCheck="false" type='text' onChange={this.handlephoneNumberInputChange} value={this.state.phoneNumber}></input>
+          <input className='ageInput formInput' spellCheck="false" type='text' onChange={this.handleAgeInputChange} value={this.state.age}></input>
+          <input className='inputButton' type='submit' value='Add' />
+        </form>
       </div>
     );
   }
